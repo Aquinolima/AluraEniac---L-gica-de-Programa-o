1e6b657d5f3a802b55ca3dffb78b8005b409d924
Thiago Aquino <aquinolima.thiago@gmail.com>
2020-04-04 02:35:42 -0300

    Curso C I - Aula 02 – Condicionais e Loops com Ifs, Fors e Whiles

--- Curso C I - Introdução à Linguagem das Linguagens/adivinhacao.c
+++ Curso C I - Introdução à Linguagem das Linguagens/adivinhacao.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include<stdlib.h>
 int main(){
     // imprime o cabecalho do jogo
     printf("*******************************************\n");
@@ -8,11 +7,42 @@ int main(){
 
     int numerosecreto = 42;
     int chute;
+    int tentativas = 1;
 
-    printf("Qual eh o seu chute?  ");
-    scanf("%d", &chute);
-    printf("seu chute foi %d \ns", chute);
+    while(1 ){
 
-    system("pause");
+        printf("Tentativa %d! \n", tentativas);
+        printf("Qual eh o seu chute?  ");
+        scanf("%d", &chute);
+        printf("seu chute foi %d \n", chute);
+
+        if (chute < 0){
+            printf("Voce nao pode chutar numeros negativos!\n");
+
+            continue;
+        }
+
+        int acertou = chute == numerosecreto;
+        int maior = chute > numerosecreto;
+
+        if(acertou){
+            printf("Voce acertou, Parabens! \n ");
+            printf("Jogue novamente, voce eh um bom jogador! \n ");
+
+            break;
+        }
+        else if(maior){
+            printf("Voce errou!\n");
+            printf("Seu chute foi maior que o numero secreto!\n");
+            }
+        else {
+            printf("Voce errou!\n");
+            printf("Seu chute foi menor que o numero secreto!\n");
+            }
+        tentativas++;
+
+    }
+    printf("Fim de jogo!\n");
+    printf("Voce acertou em %d tentativas!\n", tentativas);
     return 0;
 }
